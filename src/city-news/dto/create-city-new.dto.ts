bd217ae6215ae7b3cf68d5f0a1c55f7cf6ac9601
Ibrohim Fattohov Chaqqon o'g'li <Ibrohim.fattohov@davrbank.local>
2025-10-22 16:57:14 +0500

    fix

--- src/city-news/dto/create-city-new.dto.ts
+++ src/city-news/dto/create-city-new.dto.ts
@@ -1,30 +1,73 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsString } from 'class-validator';
-import { City } from 'generated/prisma';
+import { Type } from 'class-transformer';
+import { IsArray, IsOptional, IsString } from 'class-validator';
+import { City, CityNews } from 'generated/prisma';
 
-export class CreateCityNewDto implements Omit<City, 'id'> {
+export class CreateCityNewDto implements Omit<CityNews, 'id'> {
   @ApiProperty()
   @IsString()
   @IsOptional()
-  about_en: string;
-
-  @ApiProperty()
-  @IsString()
-  @IsOptional()
-  about_oz: string;
+  desc_en: string;
 
   @ApiProperty()
   @IsString()
   @IsOptional()
-  about_ru: string;
+  desc_oz: string;
 
   @ApiProperty()
   @IsString()
   @IsOptional()
-  about_uz: string;
+  desc_ru: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  desc_uz: string;
 
   @ApiProperty()
   @IsString()
   @IsOptional()
   image: string;
+
+  @ApiProperty({
+    type: [String],
+    required: false,
+    description: 'Array of strings',
+  })
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  @Type(() => String)
+  inner_images: string[];
+
+  @ApiProperty({
+    type: [String],
+    required: false,
+    description: 'Array of strings',
+  })
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  @Type(() => String)
+  tags: string[];
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  title_en: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  title_oz: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  title_ru: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  title_uz: string;
 }
